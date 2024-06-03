61893074d95978d56384a6213c4d26e90cb04cb5
DimasWahyuH <dimaswh19@gmail.com>
2024-06-03 15:53:18 +0700

    Menambahkan program utama

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -5,3 +5,19 @@
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {0, 4, 2};
+        //pesan array integer 3 elemen
+        cout<< data.at(5)<<endl;
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang terakhir" << endl; 
+    /*penanda 2: bahwa program berjalan tanpa berhenti*/
+}
