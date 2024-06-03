1ed9786f905ed20d895be01e52438eafbc491c72
DimasWahyuH <dimaswh19@gmail.com>
2024-06-03 15:22:18 +0700

    Menambahkan 2 catch untuk blok yang akan di eksekusi dengan cout dan catch untuk jika selain integer maka akan di eksekusi dengan cout

--- ManualException.cpp
+++ ManualException.cpp
@@ -8,5 +8,14 @@ int main()
         //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan di eksekusi" << endl;
     }
+    catch (int a) {
+        //blok ini akan di eksekusi
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini akan di eksekusi*/
+        cout << "default pengecualian di eksekusi" << endl;
+    }
 
+    return 0;
 }
